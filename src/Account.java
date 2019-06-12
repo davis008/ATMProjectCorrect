697d3e0306917817e792aa1e55c0038a684c7052
davis008 <davisomondi008@gmail.com>
2019-06-12 15:38:16 +0300

    project

---
+++ src/Account.java
@@ -0,0 +1,104 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Account {
+    Scanner input = new Scanner(System.in);
+    // Create a DecimalFormat object for formatting output.
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    /* Set the customer number */
+
+    public int setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+        return customerNumber;
+    }
+
+    /* Get the customer number */
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    /* Set the pin number */
+
+    public int setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+        return pinNumber;
+    }
+
+    /* Get the pin number */
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    /* Get Saving Account Balance */
+
+    public double getSavingBalance() {
+        return Balance;
+    }
+
+
+
+    /* Calculate Saving Account withdrawal */
+
+    public double calcSavingWithdraw(double amount) {
+        Balance = (Balance - amount);
+        return Balance;
+    }
+
+
+    /* Calculate Saving Account deposit */
+
+    public double calcSavingDeposit(double amount) {
+        Balance = (Balance + amount);
+        return Balance;
+    }
+
+
+    /* Customer Saving Account Withdraw input */
+
+    public void getSavingWithdrawInput() {
+        for(count=0;count<4;count++) {
+            System.out.println("Saving Account Balance: " + moneyFormat.format(Balance));
+            System.out.print("Amount you want to withdraw from Saving Account: ");
+            double amount = input.nextDouble();
+            if ((amount < TRANSACTION_WITHDRAWAL_LIMIT && (amount < DAILY_WITHDRAWAL_LIMIT)) && ((Balance - amount) >= 0)) {
+                calcSavingWithdraw(amount);
+                System.out.println("New  Account balance: " + Balance + "\n");
+            } else {
+                System.out.println("Balance cannot be Negative");
+            }
+        }
+        }
+
+    /* Customer Saving Account Deposit input */
+    public void getSavingDepositInput() {
+        for(count=0;count<4;count++) {
+
+            if (Balance > 0) {
+                System.out.println("Saving Account Balance: " + moneyFormat.format(Balance));
+            }
+
+            System.out.print("Enter Amount you want to deposit from Saving Account: ");
+            double amount = input.nextDouble();
+            if (((amount < TRANSACTION_DEPOSIT_LIMIT && (amount < DAILY_DEPOSIT_LIMIT)) && ((Balance + amount) >= 0))) {
+                calcSavingDeposit(amount);
+                System.out.println("New  Account balance: " + Balance + "\n");
+            } else {
+                System.out.println("Balance cannot be Negative");
+            }
+        }
+        }
+    private int customerNumber;
+    private int count;
+    private int pinNumber;
+    private double Balance = 0;
+
+    private final int DAILY_DEPOSIT_LIMIT = 150000;
+    private final int TRANSACTION_DEPOSIT_LIMIT = 40000;
+
+    private final int DAILY_WITHDRAWAL_LIMIT = 50000;
+    private final int TRANSACTION_WITHDRAWAL_LIMIT = 20000;
+
+}
---
+++ src/OptionMenu.java
@@ -0,0 +1,87 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class OptionMenu extends Account {
+    Scanner menuInput = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+
+    /* Validate Login information customer number and pin number */
+
+    public void getLogin() throws IOException {
+        int x = 1;
+
+        do {
+            try {
+                /*
+                 * Accounts in a HashMap, key = customer number, value = pin
+                 * number
+                 */
+                data.put(1234, 1234);
+                data.put(6780, 1230);
+
+
+                System.out.print("Enter Your Customer Number: ");
+                setCustomerNumber(menuInput.nextInt());
+
+                System.out.print("Enter Your Pin Number: ");
+                setPinNumber(menuInput.nextInt());
+            } catch (Exception e) {
+                System.out.println("\n" + "Invalid character(s). Only numbers." + "\n");
+                x = 2;
+            }
+            for (Entry<Integer, Integer> entry : data.entrySet()) {
+                if (entry.getKey() == getCustomerNumber() && entry.getValue() == getPinNumber()) {
+                    getSaving();
+                }
+            }
+            System.out.println("\n" + "Wrong Customer Number or Pin Number." + "\n");
+        } while (x == 1);
+    }
+
+
+        /* Display Saving Account Menu with selections */
+
+        public void getSaving() {
+            System.out.println("Account: ");
+            System.out.println("Type 1 - Balance");
+            System.out.println("Type 2 - Withdrawal");
+            System.out.println("Type 3 - Deposit");
+            System.out.println("Type 4 - Quit");
+            System.out.print("Menu: ");
+
+            selection = menuInput.nextInt();
+
+            switch (selection) {
+                case 1:
+                    System.out.println("Saving Account Balance: " + moneyFormat.format(getSavingBalance()) + "\n");
+                    break;
+
+                case 2:
+                    getSavingWithdrawInput();
+                    break;
+
+                case 3:
+                    getSavingDepositInput();
+                    break;
+
+                case 4:
+                    System.out.println("Do you want to quit?");
+                    Scanner scan = new Scanner(System.in);
+                    choice = scan.nextLine();
+                    System.out.println();
+                    break;
+
+                default:
+                    System.out.println("\n" + "Invalid choice." + "\n");
+                    getSaving();
+            }
+        }
+
+        int selection;
+        String choice = "y";
+    }
